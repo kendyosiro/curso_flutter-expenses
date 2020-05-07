fb6fbcb1767e2a8eca176a3580e90631caadd949
Kendy Osiro <kendyosiro@gmail.com>
2020-05-07 15:16:33 -0300

    Ao digitar o titulo ou valor, submete a transacao. No campo numerico, abre o teclado numerico com ponto tb para o iphone.

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -8,43 +8,53 @@ class TransactionForm extends StatelessWidget {
 
   TransactionForm(this.onSubmit);
 
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+    
+    if(title.isEmpty || value <=0 ){
+      return;
+    }
+
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(
-                      labelText: 'Título',
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      FlatButton(
-                        child: Text('Nova transação'),
-                        textColor: Colors.purple,
-                        onPressed: () {
-                          final title = titleController.text;
-                          final value = double.tryParse(valueController.text) ?? 0.0;
-                          onSubmit(title, value);
-                        },
-                      ),
-                    ],
-                  ),
-                ],
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: titleController,
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(
+                labelText: 'Título',
               ),
             ),
-          );
+            TextField(
+              controller: valueController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(
+                labelText: 'Valor (R\$)',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Nova transação'),
+                  textColor: Colors.purple,
+                  onPressed: _submitForm,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
